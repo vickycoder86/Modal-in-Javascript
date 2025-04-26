fd391a34d292d109f75fd9dfbca6f4e8a3296727
vickycoder86 <vijay.jainm1986@gmail.com>
2025-04-26 15:33:24 +0530

    change 2

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ let removemodal = document.querySelector(".modal h2 span");
 
 let toggle_button = document.querySelector(".toggle_button");
 
+let showpasswordBtn = document.querySelector(".password_button");
+let password = document.querySelector(".password");
+
 btn.addEventListener("click", () => {
   modal.classList.add("showmodal");
 });
@@ -13,3 +16,23 @@ removemodal.addEventListener("click", () => {
 toggle_button.addEventListener("click", () => {
   modal.classList.toggle("showmodal");
 });
+showpasswordBtn.addEventListener("click", () => {
+  if ((showpasswordBtn.innerHTML == "Show Password")) {
+    password.type = "text";
+    showpasswordBtn.innerHTML = "Hide Password";
+  }else{
+    password.type = "password";
+    showpasswordBtn.innerHTML = "Show Password";
+  }
+});
+
+//using function
+// function showpasswords() {
+//   if ((showpasswordBtn.innerText == "Show Password")) {
+//     password.type = "text";
+//     showpasswordBtn.innerHTML = "Hide Password";
+//   } else {
+//     password.type = "password";
+//     showpasswordBtn.innerHTML = "Show Password";
+//   }
+// }
