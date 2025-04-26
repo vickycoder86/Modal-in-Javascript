60327b76f8f86fa70df201469569b162c46217c6
vickycoder86 <vijay.jainm1986@gmail.com>
2025-04-26 14:44:10 +0530

    change 2

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ let btn = document.querySelector(".show_button");
 let modal = document.querySelector(".modal");
 let removemodal = document.querySelector(".modal h2 span");
 
+let toggle_button = document.querySelector(".toggle_button");
+
 btn.addEventListener("click", () => {
   modal.classList.add("showmodal");
 });
 removemodal.addEventListener("click", () => {
   modal.classList.remove("showmodal");
+});
+toggle_button.addEventListener("click", () => {
+  modal.classList.toggle("showmodal");
 });
